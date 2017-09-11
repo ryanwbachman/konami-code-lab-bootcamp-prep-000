4aefc40755d14fdab5fa60675610019729f0584f
ryanwbachman <ryanwbachman@gmail.com>
2017-09-11 01:31:48 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,31 +3,31 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   console.log('initialize');
 
-  document.body.addEventListener('keydown', function (e) {
-      alert('hello world');
-  });​​​​​​​
-
-  const alphabet = [65, 66, 67];
-
-  // Keep track of index outside of the event handler.
-  let index = 0;
-
-  // This is the function that would be invoked by the event listener.
-  function onKeyDownHandler(e) {
-    const key = parseInt(e.detail || e.which);
-    console.log(key);
-
-    if (key === alphabet[index]) {
-      index++;
-
-      if (index === alphabet.length) {
-        alert("Hurray!");
-
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
-  }
+  // document.body.addEventListener('keydown', function (e) {
+  //     alert('hello world');
+  // });​​​​​​​
+  // 
+  // const alphabet = [65, 66, 67];
+  // 
+  // // Keep track of index outside of the event handler.
+  // let index = 0;
+  // 
+  // // This is the function that would be invoked by the event listener.
+  // function onKeyDownHandler(e) {
+  //   const key = parseInt(e.detail || e.which);
+  //   console.log(key);
+  // 
+  //   if (key === alphabet[index]) {
+  //     index++;
+  // 
+  //     if (index === alphabet.length) {
+  //       alert("Hurray!");
+  // 
+  //       index = 0;
+  //     }
+  //   } else {
+  //     index = 0;
+  //   }
+  // }
 
 }
