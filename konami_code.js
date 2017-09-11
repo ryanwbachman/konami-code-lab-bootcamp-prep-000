f3b44e0a344f23bbffc0a32edaf94d8b69fa704d
ryanwbachman <ryanwbachman@gmail.com>
2017-09-11 01:34:35 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,9 +3,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 function init() {
   console.log('initialize');
 
-  // document.body.addEventListener('keydown', function (e) {
-  //     alert('hello world');
-  // });​​​​​​​
+  document.addEventListener('keydown', function (e) {
+      alert('hello world');
+  });​​​​​​​
   //
   // const alphabet = [65, 66, 67];
   //
