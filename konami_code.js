a856b3058d33bec43ec9b9c38610626184ed1332
ryanwbachman <ryanwbachman@gmail.com>
2017-09-11 01:48:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -8,27 +8,4 @@ function init() {
   document.body.addEventListener('click', function () {
       console.alert('hello world');
   });​
-    // const alphabet = [65, 66, 67];
-  //
-  // // Keep track of index outside of the event handler.
-  // let index = 0;
-  //
-  // // This is the function that would be invoked by the event listener.
-  // function onKeyDownHandler(e) {
-  //   const key = parseInt(e.detail || e.which);
-  //   console.log(key);
-  //
-  //   if (key === alphabet[index]) {
-  //     index++;
-  //
-  //     if (index === alphabet.length) {
-  //       alert("Hurray!");
-  //
-  //       index = 0;
-  //     }
-  //   } else {
-  //     index = 0;
-  //   }
-  // }
-
 }
